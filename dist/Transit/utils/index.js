393b7067555327fbc8e17819172d10ac376230f4
Tawfiq Jawhar <tawjaw@gmail.com>
2022-09-02 02:39:44 -0400

    removed dist that was added by accident

--- dist/Transit/utils/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.isString = void 0;
-function isString(value) {
-    return typeof value === 'string' || value instanceof String;
-}
-exports.isString = isString;
